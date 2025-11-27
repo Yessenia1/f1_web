58caf5567771a5bff1ea5fc0ff671674a7aa84bd
Yessenia1 <84026815+Yessenia1@users.noreply.github.com>
2025-11-27 11:51:04 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,17 @@
 from flask import Flask, render_template, request, redirect, session
 import sqlite3
+import mysql.connector
 
 app = Flask(__name__)
 app.secret_key = "supersecret"
 
 def get_db():
-    conn = sqlite3.connect("database.db")
-    conn.row_factory = sqlite3.Row
-    return conn
+    return mysql.connector.connect(
+        host="db",
+        user="f1user",
+        password="f1pass",
+        database="f1db"
+    )
 
 @app.route("/")
 def index():
